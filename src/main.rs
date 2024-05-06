f7c07bb93b6e0a36934ecd849923006cb25a54aa
WeaponizedSchizophrenia <laurynasramankevicius2@gmail.com>
2024-05-06 18:01:39 +0300

    Added logging.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use log4rs::config::Deserializers;
+
 fn main() {
-    println!("Hello, world!");
+    match init_logging() {
+        Ok(_) => log::info!("Logging intialized"),
+        Err(e) => {
+            eprintln!("Failed to initialize logging: {}", e);
+            println!("The application will continue without logging.");
+        }
+    }
+}
+
+fn init_logging() -> Result<(), anyhow::Error> {
+    log4rs::init_file("./config/log.yaml", Deserializers::default())
 }
